0c15bdd729a1e3d6168e261ff06d1723c62ebde0
malpish0n <akasztelan617@gmail.com>
2026-02-20 11:50:07 +0100

    git init and add basic spring structure

---
+++ src/main/java/com/malpishon/gameflow/GameflowApplication.java
@@ -0,0 +1,13 @@
+package com.malpishon.gameflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameflowApplication.class, args);
+	}
+
+}
