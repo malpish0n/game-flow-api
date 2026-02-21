3de2b0eb1b6e3e093cb4d365b00da82f8ec45288
malpish0n <akasztelan617@gmail.com>
2026-02-21 16:07:32 +0100

    add GameService

--- src/main/java/com/malpishon/gameflow/game/GameService.java
+++ src/main/java/com/malpishon/gameflow/game/GameService.java
@@ -2,7 +2,22 @@ package com.malpishon.gameflow.game;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class GameService {
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
+
+    public List<Game> findAllGames() {
+        return gameRepository.findAll();
+    }
+
+    public Game saveGame(Game game) {
+        return gameRepository.save(game);
+    }
 
 }
